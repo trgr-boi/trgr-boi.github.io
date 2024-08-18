ad9012c4e6f0520eb3675f5c47a4044486267800
Trigger-BOI <tuurlammes1@gmail.com>
2024-08-18 22:24:52 +0200

    test-code added

--- script-main-page.js
+++ script-main-page.js
@@ -3,8 +3,10 @@
 
     var outputDiv = document.getElementById('output'),
         inputField = document.getElementById('command-input');
-    var commandHistory = []; // Array to store the command history
-    var historyIndex = -1; // Index to track the current position in the command history
+    var commandHistory = [];
+    var historyIndex = -1;
+    var awaitingPassword = false; // Track if the terminal is waiting for password input
+    var currentCommand = ''; // Store the current command
 
     document.body.addEventListener('click', function() {
         var textarea = document.getElementById('command-input');
@@ -14,74 +16,72 @@
         var textarea = document.getElementById('command-input');
         textarea.focus(); // Set focus to the textarea
     });
-    
+
     inputField.addEventListener('keydown', function (event) {
         if (event.key === 'Enter') {
             var command = inputField.value;
             inputField.value = '';
-            executeCommand(command);
-        } else
-        if (event.key === 'ArrowUp') {
-            // Navigate to the previous command in history
+            if (awaitingPassword) {
+                checkPassword(command); // Handle password input
+            } else {
+                executeCommand(command); // Handle regular commands
+            }
+        } else if (event.key === 'ArrowUp') {
             navigateCommandHistory(-1);
-        } else 
-        if (event.key === 'ArrowDown') {
-            // Navigate to the next command in history
+        } else if (event.key === 'ArrowDown') {
             navigateCommandHistory(1);
         }
     });
 
-   function executeCommand(command) {
-        {
-        // Echo of typed command
+    function executeCommand(command) {
+
         appendOutput('>guest/~terminal.OS//: ' + command + '<br>', 'color2');
-        }
-       
-        // Add the command to the history
         commandHistory.push(command);
         historyIndex = commandHistory.length;
-       
-       // commands & reactions
+
         if (command.toLowerCase() === 'help') {
             showHelp();
-        } else
-        if (command.toLowerCase() === 'about') {
+        } else if (command.toLowerCase() === 'about') {
             showAbout();
-        } else
-        if (command.toLowerCase() === 'whoami') {
+        } else if (command.toLowerCase() === 'whoami') {
             showWhoami();
-        } else 
-        if (command.toLowerCase() === 'clear'){
+        } else if (command.toLowerCase() === 'clear') {
             clearOutput();
-        } else
-        if (command.toLowerCase() === 'music'){
+        } else if (command.toLowerCase() === 'music') {
             window.location.href = 'music.html';
-        } else
-        if (command.toLowerCase() === 'old-music'){
+        } else if (command.toLowerCase() === 'old-music') {
             window.location.href = 'old-music.html';
-        } else
-         if (command.toLowerCase() === 'files') {
-            showFiles(); //make link later
-        } else
-        if (command.toLowerCase() === 'depo'){
+        } else if (command.toLowerCase() === 'files') {
+            showFiles();
+        } else if (command.toLowerCase() === 'depo') {
             window.open('https://github.com/Trigger-BOI');
-        } else
-        if (command.toLowerCase() === 'liefje'){
+        } else if (command.toLowerCase() === 'liefje') {
             showLiefje();
-        } else
-        if (command.toLowerCase() === 'suki'){
+        } else if (command.toLowerCase() === 'suki') {
             showSuki();
-        } else
-
-        {
-            // Unknown command message
-        appendOutput("Unknown command: '" + command + "'.<br>Type '");
-        appendOutput("help", 'color1');
-        appendOutput("' for available commands.<br>");
+        } else if (command.toLowerCase() === 'test-code') {
+            appendOutput('Enter the password to access:<br><br>', 'color1');
+            awaitingPassword = true;
+            currentCommand = command;
+        } else {
+            appendOutput("Unknown command: '" + command + "'.<br>Type '");
+            appendOutput("help", 'color1');
+            appendOutput("' for available commands.<br>");
         }
-       
-   }
-    
+    }
+
+    function checkPassword(password) {
+        const correctPassword = "0000";
+        if (password === correctPassword) {
+            appendOutput('Access granted.<br><br>', 'color1');
+            showTestCode(); // Execute the command
+        } else {
+            appendOutput('Incorrect password. Access denied.<br><br>', 'color1');
+        }
+        awaitingPassword = false; // Reset the state
+        currentCommand = ''; // Clear the current command
+    }
+
     function navigateCommandHistory(offset) {
         var newIndex = historyIndex + offset;
         if (newIndex >= 0 && newIndex < commandHistory.length) {
@@ -89,14 +89,13 @@
             inputField.value = commandHistory[historyIndex];
         }
     }
-    
+
     function showHelp() {
-        // Display help information
         appendOutput('Available commands:<br>');
         appendOutput('about', 'color1');
         appendOutput(' - find out more about this.<br>');
         appendOutput('whoami', 'color1');
-        appendOutput(' - Who even am i?<br>');
+        appendOutput(' - Who even am I?<br>');
         appendOutput('depo', 'color1');
         appendOutput(' - go to Github page.<br>');
         appendOutput('music', 'color1');
@@ -107,24 +106,27 @@
         appendOutput(' - You clearly know how to use this.<br>');
         appendOutput('clear', 'color1');
         appendOutput(' - clear the terminal.<br>')
-        // Add more help messages for other commands as needed
     }
+
     function showAbout() {
         appendOutput('Welcome to the terminal.<br>');
         appendOutput('This is a place where I will use to place my mark.<br><br>');
-        appendOutput('right now, there is only some music to find. This is there purely for testing reasons. ')
-        appendOutput('Your are only here for test reasons...<br>', 'color1')
-        appendOutput('Explore further.<br>')
+        appendOutput('Right now, there is only some music to find. This is there purely for testing reasons. ');
+        appendOutput('You are only here for test reasons...<br>', 'color1');
+        appendOutput('Explore further.<br>');
     }
+
     function showWhoami() {
-        appendOutput("Are you asking me or yourself?<br><br>")
-        appendOutput("I am a simple living and learning creature.<br>")
-        appendOutput("I go by the name TRGR on here. <br><br>")
-        appendOutput("I don't fully know about it all... <br>")
+        appendOutput("Are you asking me or yourself?<br><br>");
+        appendOutput("I am a simple living and learning creature.<br>");
+        appendOutput("I go by the name TRGR on here. <br><br>");
+        appendOutput("I don't fully know about it all... <br>");
     }
+
     function showFiles() {
-        appendOutput('Files are still uploading...<br>Try again later.<br>')
+        appendOutput('Files are still uploading...<br>Try again later.<br>');
     }
+
     function clearOutput() {
         outputDiv.innerHTML = ''; // Clears the content of the output div
     }
@@ -133,7 +135,7 @@
         outputDiv.innerHTML += '<span class="' + className + '">' + output + '</span>';
         outputDiv.scrollTop = outputDiv.scrollHeight;
     }
-    
+
     function showLiefje() {
         appendOutput('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⣤⣤⣤⣤⣤⣤⣤⣤⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀<br>⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⠶⠟⠛⠛⠉⠉⠉⠁⠀⠀⠈⠉⠉⠙⠛⠿⢶⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀<br>⠀⠀⠀⠀⠀⠀⣠⠖⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⢷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀<br>⠀⠀⠀⠀⢠⡾⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢷⣄⠀⠀⠀⠀⠀⢀⣀⠀⠀⠀⠀<br>⠀⠀⠀⣠⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⠀⠀⠀⠹⣧⡀⠀⠀⠀⡏⠉⠙⠋⢹⡆<br>⠀⠀⢠⡿⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠚⠉⠉⠛⠿⣦⡀⠀⠸⣷⡀⠀⠀⠹⣄⠀⠠⡹⠃<br>⠀⢀⣿⠃⠀⠀⠀⠀⣠⣶⠾⠟⠛⠉⠉⠛⠿⣶⣄⠀⠀⠀⠀⣰⠃⢀⣠⣤⣄⠀⠈⢻⣦⠀⢻⣧⠀⠀⠀⠈⠓⠋⠀⠀<br>⠀⢸⣿⠀⠀⠀⣠⣾⠋⠁⠀⠀⢀⣠⣤⣤⣤⣈⠙⣷⣄⠀⢠⣏⣶⣿⣿⣿⣿⣿⣦⠀⢻⣧⠘⣿⢀⢸⡛⠛⢲⠀⠀⠀<br>⠀⣾⡇⠀⠀⢠⡿⠁⠀⠀⣠⣾⡿⢿⣿⣿⣿⣿⣷⣌⢿⣆⢸⣿⡋⠉⢉⣿⣿⣿⣿⣧⠈⣿⠀⣿⣿⠀⠹⠶⠋⠀⠀⠀<br>⠈⡟⣿⠀⠀⢸⠇⠀⢀⣼⣄⠈⠀⣸⣿⣿⣿⣿⣿⣿⡎⣿⣿⣿⣿⣶⣿⣻⣿⣿⣿⣿⣤⣿⢀⣿⣿⠀⠀⠀⠀⠀⠀⠀<br>⠀⢷⢹⡆⠀⢸⡄⠀⣾⣿⣿⣿⣿⣿⣩⣿⣿⣿⣿⣿⣇⣿⢹⣿⣿⣿⣿⣿⣿⣿⣟⣿⣿⠏⣸⢧⠏⠀⠀⠀⠀⠀⠀⠀<br>⠀⠈⣇⢳⡀⠈⢧⠐⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⠏⢠⢁⡞⠀⠀⠀⠀⠀⠀⠀⠀<br>⠀⠀⠈⢎⠃⠀⠈⠳⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣾⠟⠁⠀⠀⠈⠻⣿⣿⣿⣿⡿⠟⠁⠀⣠⠎⠀⠀⠀⠀⠀⠀⠀⠀⠀<br>⠀⠀⠀⠈⠳⣄⠀⠀⠈⠙⡻⠿⢿⣿⡿⠿⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠈⠉⠃⣀⡀⣰⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀<br>⠀⠀⠀⣀⡤⠬⣷⣄⡲⣤⣈⠀⠈⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⡴⢛⡵⠚⠁⠀⢀⣠⣤⣀⡀⠀⠀⠀⠀⠀⠀<br>⠀⠀⠸⣇⠀⠀⠀⢻⣿⠒⠭⣟⣷⢶⣤⣤⣤⣤⣤⣤⣤⣤⣶⢾⣟⠯⠗⠊⠁⠀⠀⠀⣾⠟⠁⠀⢘⣿⠀⠀⠀⠀⠀⠀<br>⠀⠀⠀⢹⣧⠀⠀⠈⣿⡄⠀⠀⠈⠉⠛⠛⠛⠛⠛⠒⠋⠛⠉⠉⠀⠀⠀⠀⠀⠀⠀⢸⡿⠀⠀⢠⣿⠏⠀⠀⠀⠀⠀⠀<br>⠀⠀⠀⣠⣿⠇⠀⠀⠙⠛⠛⠒⠒⠛⠛⠛⠻⠿⢷⡢⣤⣔⡶⠿⠟⠛⠛⠛⠻⠿⠽⠟⠁⠀⠀⢺⡿⡄⠀⠀⠀⠀⠀⠀<br>⠀⠀⢰⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣾⡁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⡀⠀⠀⠀⠀⠀<br>⠀⠀⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠙⠛⠻⢿⣿⠒⠲⠛⠙⠲⠦⢼⣿⡿⠟⠛⠃⠀⠀⠀⠀⠀⠀⠀⢸⣇⡇⠀⠀⠀⠀⠀<br>⠀⠀⠹⠿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠇⠀⠀⠀⠀⠀⢸⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡾⡻⠃⠀⠀⠀⠀⠀<br>⠀⠀⠀⠀⠈⠻⢦⣀⠀⠀⠀⠀⠀⠀⢀⡾⡿⠀⠀⠀⠀⠀⠀⠘⣿⣆⠀⠀⠀⠀⠀⠀⢀⣠⣴⢯⠞⠀⠀⠀⠀⠀⠀⠀<br>⠀⠀⠀⠀⠀⠀⠀⠉⠛⠳⠶⠶⠶⣖⡿⠚⠁⠀⠀⠀⠀⠀⠀⠀⠘⠮⣷⣶⡤⡤⠴⠾⠟⠋⠚⠁⠀⠀⠀⠀⠀⠀⠀⠀<br>⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀<br>')
     }
@@ -142,5 +144,9 @@
         appendOutput('Hey liefje<br><br>Ik zie jou zo suuuuper graag. Sorry dat dit er niet van het begin <br>in verstopt zat voor jou.<br>')
         appendOutput('Dankje voor mij leukde ideetjes the geven voor dingen te verstoppen :)<br>I love YOU <333<br><br>')
         appendOutput('Veel liede,<br>Jouw liefje<br><br>')
+    }
+
+    function showTestCode() {
+        appendOutput('Test succesful<br>')
     }
 })();
