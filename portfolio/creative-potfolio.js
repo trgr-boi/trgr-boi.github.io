d2616b29add474bb8abd144ddbb48b7414c0e57c
Trigger-BOI <tuurlammes1@gmail.com>
2024-09-22 19:22:16 +0200

    loading screed for when images load

---
+++ portfolio/creative-potfolio.js
@@ -0,0 +1,5 @@
+// Wait for the window to load (including images, styles, etc.)
+window.addEventListener("load", function() {
+    // Hide the loading screen
+    document.getElementById("loading-screen").style.display = "none";
+});
