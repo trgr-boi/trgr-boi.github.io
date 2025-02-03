b2fe4b6ee46053d0c6f65405001cace1a6efcb80
Trigger-BOI <tuurlammes1@gmail.com>
2025-02-03 16:54:19 +0100

    site overhaul

---
+++ main-site/src/js/change-page.js
@@ -0,0 +1,11 @@
+function loadContent(page) {
+    fetch('pages/' + page)
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById('content').innerHTML = html;
+        })
+        .catch(error => console.error('Error loading content:', error));
+};
+
+// Load default page
+window.onload = () => loadContent('home.html');
