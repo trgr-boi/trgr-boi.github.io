1005040f1dc702b7b1c607a938a524715e6d4111
Trigger-BOI <tuurlammes1@gmail.com>
2025-02-03 17:10:11 +0100

    Fixed images, and page load js

--- trgr-notes/src/js/change-page.js
+++ trgr-notes/src/js/change-page.js
@@ -3,6 +3,7 @@ function loadContent(page) {
         .then(response => response.text())
         .then(html => {
             document.getElementById('content').innerHTML = html;
+            window.scrollTo(0, 0); // Scroll to the top of the page
         })
         .catch(error => console.error('Error loading content:', error));
 };
