28dc45498cf47cb223049cdbc2f238ef2cae7cda
Trigger-BOI <tuurlammes1@gmail.com>
2025-03-16 22:06:13 +0100

    prettier applied

--- main-site/src/js/change-page.js
+++ main-site/src/js/change-page.js
@@ -1,11 +1,11 @@
 function loadContent(page) {
-    fetch('pages/' + page)
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById('content').innerHTML = html;
+    fetch("pages/" + page)
+        .then((response) => response.text())
+        .then((html) => {
+            document.getElementById("content").innerHTML = html;
         })
-        .catch(error => console.error('Error loading content:', error));
-};
+        .catch((error) => console.error("Error loading content:", error));
+}
 
 // Load default page
-window.onload = () => loadContent('home.html');
+window.onload = () => loadContent("home.html");
--- main-site/src/js/shortcuts.js
+++ main-site/src/js/shortcuts.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.addEventListener('keydown', function(event) {
-        if (event.key === 'n' || event.key === 'N') {
-            const toggleButton = document.getElementById('toggleDisplayBtn');
+document.addEventListener("DOMContentLoaded", function () {
+    document.addEventListener("keydown", function (event) {
+        if (event.key === "n" || event.key === "N") {
+            const toggleButton = document.getElementById("toggleDisplayBtn");
             toggleButton.click();
-        } else if (event.key === 't' || event.key === 'T') {
+        } else if (event.key === "t" || event.key === "T") {
             toggleTheme();
         }
     });
