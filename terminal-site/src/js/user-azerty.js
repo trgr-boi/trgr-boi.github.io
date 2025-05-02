8e96d862bf494a2ab36f19b4d9995a6419dceca4
Trigger-BOI <tuurlammes1@gmail.com>
2025-05-03 00:36:00 +0200

    Removed subliminal.os

--- terminal-site/src/js/user-azerty.js
+++
@@ -1,8 +0,0 @@
-function commandsAzerty(command) {
-    if (command === "help -p") showHelpAzerty();
-    else unknownCommand(command);
-}
-
-function showHelpAzerty() {
-    appendOutput("You don't have any programs installed.<br>");
-}
